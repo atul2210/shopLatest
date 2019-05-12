ba330d9d7fc452ea642af958e99308e8c6e34c73
atul.s2210 <atul.s2210@outlook.com>
2019-05-12 14:10:40 +0530

    connection string

---
+++ ShoppingApi/Model/ErrorLogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingApi.Model
+{
+    public class ErrorLogger
+    {
+        public int id { get; set; }
+        public string ErrorDescription { get; set; }
+    }
+}
