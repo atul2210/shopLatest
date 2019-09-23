d8a245a01511ee6e6f2e66cb5c765aace6dde8d0
Your Namedelhi <you@example.com>
2019-09-23 22:58:18 +0530

    custom ipchecker middleware

--- ShoppingApi/Common/IpCheckerMiddleware.cs
+++ ShoppingApi/Common/IpCheckerMiddleware.cs
@@ -4,7 +4,7 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.Extensions.Configuration;
 namespace ShoppingApi.Common
 {
     public class IpCheckerMiddleware
@@ -12,21 +12,27 @@ namespace ShoppingApi.Common
         private readonly RequestDelegate _next;
         private readonly ILogger<IpCheckerMiddleware> _logger;
         private readonly string _adminSafeList;
-
         private readonly RequestDelegate next;
-        public IpCheckerMiddleware(RequestDelegate next, ILogger<IpCheckerMiddleware> logger, string adminWhiteList)
+        private IConfiguration _iConfiguration;
+        public IpCheckerMiddleware(RequestDelegate next, ILogger<IpCheckerMiddleware> logger, string adminWhiteList, IConfiguration iconfiguration)
         {
             _adminSafeList = adminWhiteList;
             _next = next;
             _logger = logger;
-        }
+            _iConfiguration = iconfiguration;
 
+
+        }
+     
         public  async  Task Invoke(HttpContext context)
         {
-            //if (context.Request.Method != "GET")
-            //{
-                var remoteIp = context.Connection.RemoteIpAddress;
-               // _logger.LogDebug($"Request from Remote IP address: {remoteIp}");
+            var val = context.Request.Headers["Referer"].ToString();
+            var origin = context.Request.Headers["Origin"].ToString();
+            var corectorigin = _iConfiguration.GetSection("OriginUrl").Value;
+            IPHostEntry iphostinfo = Dns.GetHostEntry(Dns.GetHostName());
+          
+            var remoteIp = context.Connection.RemoteIpAddress;
+           
 
                 string[] ip = _adminSafeList.Split(';');
 
@@ -37,12 +43,21 @@ namespace ShoppingApi.Common
                     var testIp = IPAddress.Parse(address);
                     if (testIp.GetAddressBytes().SequenceEqual(bytes))
                     {
-                    badIP = false;
+                        badIP = false;
+                        //break;
+                        await _next.Invoke(context);
                         break;
                     }
                 }
 
-                if (badIP)
+                if (badIP && origin.Trim()== corectorigin.Trim())
+                {
+
+                     await _next.Invoke(context);
+                }
+
+
+          else if (badIP)
                 {
                     _logger.LogInformation($"Forbidden Request from Remote IP address: {remoteIp}");
                     context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
@@ -50,7 +65,7 @@ namespace ShoppingApi.Common
                 }
            // }
 
-            await _next.Invoke(context);
+           
 
         }
 
