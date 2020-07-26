9b148f516629600998d7ed3d78a2b569f90b8215
Manisha Srivastava <msrivastava367@outlook.com>
2020-07-26 12:51:24 +0530

    latest script

---
+++ ShoppingApi/Common/GoogleDrive.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chilkat;
+
+namespace ShoppingApi.Common
+{
+    public class GoogleDrive
+    {
+        public OAuth2 GetGoogleDriveToken()
+        {
+
+            return null;
+        }
+    }
+}
